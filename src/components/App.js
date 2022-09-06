258358438d4688645f360e1f4481f50e0d2634c4
Flo Slv <florent3969@hotmail.fr>
2022-09-06 16:26:14 +0200

    🟦 feat: add loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Menu from './Menu.js';
 import Sidebar from './Sidebar.js';
@@ -6,16 +6,37 @@ import Sidebar from './Sidebar.js';
 import '../css/app.css';
 
 const App = () => {
+	useEffect(() => {
+		setTimeout(() => {
+			const animated = document.getElementById('animated');
+
+			animated.style.opacity = 1;
+		}, 6000);
+	});
+
 	return (
-		<div className="app">
-			<header>
-				<Menu />
-			</header>
+		<div className={'app'}>
+			<div id='loader'>
+				<div>V</div>
+				<div>L</div>
+				<div>S</div>
+				<div>-</div>
+				<div>O</div>
+				<div>L</div>
+				<div>F</div>
+			</div>
 
-			<Sidebar />
+			<div id='animated'>
+				<header>
+					<Menu />
+				</header>
 
-			<main>
-			</main>
+				<Sidebar />
+
+				<main>
+					Under construction !
+				</main>
+			</div>
 		</div>
 	);
 };
