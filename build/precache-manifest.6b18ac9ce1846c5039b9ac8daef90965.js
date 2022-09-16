0fff97c96baf55707cf87412bea5e01647386259
Flo Slv <florent3969@hotmail.fr>
2022-09-16 12:33:05 +0200

    🟦 feat: build for production

---
+++ build/precache-manifest.6b18ac9ce1846c5039b9ac8daef90965.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dae0112c7b17419c01e8",
+    "url": "/static/css/main.d2a2e7ad.chunk.css"
+  },
+  {
+    "revision": "dae0112c7b17419c01e8",
+    "url": "/static/js/main.dae0112c.chunk.js"
+  },
+  {
+    "revision": "f3ba07ae8adf9c0824d2",
+    "url": "/static/js/1.f3ba07ae.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ddd68001b6ef7d03a2f8748ad590fb8",
+    "url": "/index.html"
+  }
+];
