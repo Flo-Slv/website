683641d71aa93dbd5c57b334f85fdaa813e476c6
Flo Slv <florent3969@hotmail.fr>
2022-09-30 10:04:58 +0200

    🟦 feat: add About component + css

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../css/about.css';
+
+const About = () => {
+	return (
+		<section id='about'>
+
+		</section>
+	);
+};
+
+export default About;
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import '../css/about.css';
+import '../css/intro.css';
 
 const Intro = () => {
 	return (
