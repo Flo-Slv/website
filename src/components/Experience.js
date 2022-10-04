a2a415f9f4dbde904b10cd8b0b5c6e9aeb9b7e0c
Flo Slv <florent3969@hotmail.fr>
2022-10-04 20:00:34 +0200

    🟦 feat: Experience component + css almost done

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,18 +1,11 @@
 import React, { useState } from 'react';
 
-import Accordion from './utils/Accordion.js';
-import accordionData from './utils/accordionData.js';
-
 import '../css/experience.css';
 
 const Experience = () => {
-	const [clicked, setClicked] = useState('0');
+	const [item, setItem] = useState('jacando');
 
-	const onToggle = index => {
-		if (clicked === index) return setClicked('0');
-
-		return setClicked(index);
-	};
+	const toggle = name => setItem(name);
 
 	return (
 		<section id='experience'>
@@ -27,17 +20,60 @@ const Experience = () => {
 			</div>
 
 			<div className={'accordion'}>
-				{accordionData.map(({ title, content }, index) => (
-					<Accordion
-						key={index}
-						onToggle={() => onToggle(index)}
-						active={
-							clicked === index ? Boolean(true) : Boolean(false)
-						}
-						title={title}
-						content={content}
-					/>
-				))}
+				<div className={'accordion-menu'}>
+					<div
+						className={`accordion-title ${item === 'jacando' ? 'active-title' : ''}`}
+						onClick={() => toggle('jacando')}
+					>
+						Jacando
+					</div>
+
+					<div
+						className={`accordion-title ${item === 'aircampus' ? 'active-title' : ''}`}
+						onClick={() => toggle('aircampus')}
+					>
+						AirCampus
+					</div>
+
+					<div
+						className={`accordion-title ${item === 'teewii' ? 'active-title' : ''}`}
+						onClick={() => toggle('teewii')}
+					>
+						Teewii
+					</div>
+				</div>
+
+				<div className={'accordion-content'}>
+					<div
+						className={item === 'jacando' ? 'active-content' : 'non-active'}
+					>
+						<div className={'content-job'}>
+							Junior full-stack developer <a href='https://www.jacando.com/' target='_blanck' rel='noopener noreferrer'>@Jacando</a>
+						</div>
+						<div className={'content-date'}>Octobre 2018 a Avril 2021</div>
+						<ul>
+							<li>React JS, node.js</li>
+						</ul>
+					</div>
+
+					<div
+						className={item === 'aircampus' ? 'active-content' : 'non-active'}
+					>
+						<div className={'content-job'}>
+							Junior web developer <a href='https://aircampus.co' target='_blank' rel='noopener noreferrer'>@AirCampus</a>
+						</div>
+						<div className={'content-date'}>Mars 2017 a Octobre 2018</div>
+					</div>
+
+					<div
+						className={item === 'teewii' ? 'active-content' : 'non-active'}
+					>
+						<div className={'content-job'}>
+							Junior full-stack developer <a href='https://www.teewii.fr' target='_blank' rel='noopener noreferrer'>@Teewii</a>
+						</div>
+						<div className={'content-date'}>Octobre 2016 a Octobre 2018</div>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
