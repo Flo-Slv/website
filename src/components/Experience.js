a209d441c5e75bb3fa1bdd693254d4569dc4a9ca
Flo Slv <florent3969@hotmail.fr>
2022-10-04 08:36:07 +0200

    🟦 feat: add Experience component

---
+++ src/components/Experience.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../css/experience.css';
+
+const Experience = () => {
+	return (
+		<section id='experience'>
+			Experience
+		</section>
+	);
+};
+
+export default Experience;
