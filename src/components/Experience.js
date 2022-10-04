fba5dea3446048c68ce1703fbf30dbaeb456b892
Flo Slv <florent3969@hotmail.fr>
2022-10-04 11:32:56 +0200

    🟦 feat: Experience accordion + css

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Accordion from './utils/Accordion.js';
+import accordionData from './utils/accordionData.js';
 
 import '../css/experience.css';
 
 const Experience = () => {
+	const [clicked, setClicked] = useState('0');
+
+	const onToggle = index => {
+		if (clicked === index) return setClicked('0');
+
+		return setClicked(index);
+	};
+
 	return (
 		<section id='experience'>
-			<titleSectionRight>
+			<div className={'titleSectionRight'}>
 				<div className={'experience'}>
 					<span>02.</span> Expérience
 				</div>
@@ -13,7 +24,21 @@ const Experience = () => {
 				<div className={'vertical-line'}>
 					<hr />
 				</div>
-			</titleSectionRight>
+			</div>
+
+			<div className={'accordion'}>
+				{accordionData.map(({ title, content }, index) => (
+					<Accordion
+						key={index}
+						onToggle={() => onToggle(index)}
+						active={
+							clicked === index ? Boolean(true) : Boolean(false)
+						}
+						title={title}
+						content={content}
+					/>
+				))}
+			</div>
 		</section>
 	);
 };
