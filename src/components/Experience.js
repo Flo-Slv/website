01fe37a3f13d4844612d5221ce0616da21e71d00
Flo Slv <florent3969@hotmail.fr>
2022-10-05 10:43:45 +0200

    🟦 feat: done w/ Experience component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -25,7 +25,7 @@ const Experience = () => {
 						className={`accordion-title ${item === 'jacando' ? 'active-title' : ''}`}
 						onClick={() => toggle('jacando')}
 					>
-						Jacando
+						Jacando AG
 					</div>
 
 					<div
@@ -50,9 +50,14 @@ const Experience = () => {
 						<div className={'content-job'}>
 							Junior full-stack developer <a href='https://www.jacando.com/' target='_blanck' rel='noopener noreferrer'>@Jacando</a>
 						</div>
-						<div className={'content-date'}>Octobre 2018 a Avril 2021</div>
+						<div className={'content-date'}>Octobre 2018 à Avril 2021</div>
+						<div className={'content-city'}>Bâle - Suisse</div>
 						<ul>
-							<li>React JS, node.js</li>
+							<li>Node.js, Express.js</li>
+							<li>GraphQL, Apollo server & client</li>
+							<li>JavaScript, React.js</li>
+							<li>Je travaillais à la fois sur des tâches liées au Back-end et au Front-end. Je faisais également de l'intégration entre le Back et le Front.</li>
+							<li>A cause de la crise du Covid-19, je me suis retrouvé à 0% de temps de travail entre Février 2020 et Avril 2021, date à laquelle j'ai quitté l'entreprise.</li>
 						</ul>
 					</div>
 
@@ -62,7 +67,13 @@ const Experience = () => {
 						<div className={'content-job'}>
 							Junior web developer <a href='https://aircampus.co' target='_blank' rel='noopener noreferrer'>@AirCampus</a>
 						</div>
-						<div className={'content-date'}>Mars 2017 a Octobre 2018</div>
+						<div className={'content-date'}>Mars 2017 à Octobre 2018</div>
+						<div className={'content-city'}>Mulhouse - France</div>
+						<ul>
+							<li>Node.js, Express.js, Next.js</li>
+							<li>JavaScript, React.js</li>
+							<li>Je travaillais à la fois sur des tâches liées au Back-end et au Front-end.</li>
+						</ul>
 					</div>
 
 					<div
@@ -71,7 +82,17 @@ const Experience = () => {
 						<div className={'content-job'}>
 							Junior full-stack developer <a href='https://www.teewii.fr' target='_blank' rel='noopener noreferrer'>@Teewii</a>
 						</div>
-						<div className={'content-date'}>Octobre 2016 a Octobre 2018</div>
+						<div className={'content-date'}>Octobre 2016 à Octobre 2018</div>
+						<div className={'content-city'}>Mulhouse - France</div>
+						<ul>
+							<li>Node.js, Express.js, Next.js</li>
+							<li>JavaScript, React.js</li>
+							<li>Amazon Web Services</li>
+							<li>API: cloudinary & allMySMS</li>
+							<li>NodeCron</li>
+							<li>Internationalisation: i18n & react-i18next</li>
+							<li>Je travaillais à la fois sur des tâches liées au Back-end et au Front-end.</li>
+						</ul>
 					</div>
 				</div>
 			</div>
