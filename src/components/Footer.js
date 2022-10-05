eaba5113fb72e57a39bf918d5b7882e8fb3ebedc
Flo Slv <florent3969@hotmail.fr>
2022-10-05 10:12:07 +0200

    🟪 refactor: Improve Footer component and CSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,15 +6,11 @@ const Footer = () => {
 	return (
 		<footer>
 			<div className={'footer-desktop'}>
-				J'ai créé ce site avec du HTML, CSS et JavaScript.
-				<a
-					href='https://github.com/Flo-Slv/website'
-					target='_blank'
-					rel='noopener noreferrer'
-					aria-label='Github'
-				>
-					Code source
-				</a>
+				<div>
+					J'ai créé ce site avec du HTML, CSS et JavaScript (React.js). Je n'ai utilisé aucune librairie externe pour créer mes composants React ou le CSS.
+				</div>
+				<div>Vous pouvez retrouver le <a href='https://github.com/Flo-Slv/website' target='_blank' rel='noopener noreferrer' aria-label='Github'>code source</a> sur GitHub.
+				</div>
 			</div>
 
 			<ul className={'footer-smartphone'}>
