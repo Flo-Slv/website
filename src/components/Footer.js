b321932c3ef73c9e70320c6844edea9f74642f95
Flo Slv <florent3969@hotmail.fr>
2022-10-13 19:02:27 +0200

    🟪 refactor: Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,7 @@ const Footer = () => {
 	return (
 		<footer>
 			<div className={'footer-desktop'}>
-				<div>
-					J'ai créé ce site avec du HTML, CSS et JavaScript (React.js). Je n'ai utilisé aucune librairie externe pour créer mes composants React ou le CSS.
-				</div>
-				<div>Vous pouvez retrouver le <a href='https://github.com/Flo-Slv/website' target='_blank' rel='noopener noreferrer' aria-label='Github'>code source</a> sur GitHub.
-				</div>
+				Site fait avec <span>amour</span> !
 			</div>
 
 			<ul className={'footer-smartphone'}>
