240fd94362f84d84a8f4eb07cf8e76213d1785cd
Flo Slv <florent3969@hotmail.fr>
2022-10-25 10:08:43 +0200

    🟦 feat: small mobile css done !

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../css/menu.css';
 
 const Menu = ({ toggleModal }) => {
+	const [toggleHamburger, setToggle] = useState(Boolean(false));
+
+	const closeHamburger = () => {
+		const input = document.getElementById('hamburger-input');
+		input.checked = Boolean(false);
+		setToggle(Boolean(false));
+	};
+
 	return (
 		<nav className={'menu'}>
 			<label id='menu-hamburger'>
-				<input type='checkbox' />
-				<span className={'menu-hamburger'}>
+				<input type='checkbox' id='hamburger-input' />
+				<span
+					className={'menu-hamburger'}
+					onClick={() => setToggle(!toggleHamburger)}
+				>
 					<span className={'hamburger'}></span>
 				</span>
 
-				<ul>
-					<li><a href='#about'>À propos</a></li>
-					<li><a href='#experiences'>Expériences</a></li>
-					<li><a href='#projects'>Projets</a></li>
-					<li><a href='#contact'>Me contacter</a></li>
-					<li><a href='#resume'>CV</a></li>
+				<ul
+					className={`${toggleHamburger ? 'hamburger-open' : 'hamburger-close'}`}
+				>
+					<li onClick={() => closeHamburger()}>
+						<a href='#about'>À propos</a>
+					</li>
+					<li onClick={() => closeHamburger()}>
+						<a href='#experiences'>Expériences</a>
+					</li>
+					<li onClick={() => closeHamburger()}>
+						<a href='#projects'>Projets</a>
+					</li>
+					<li onClick={() => closeHamburger()}>
+						<a href='#contact'>Me contacter</a>
+					</li>
+					<li>
+						<a href='#resume'>CV</a>
+					</li>
 				</ul>
 			</label>
 
