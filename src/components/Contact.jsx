baf40f0489f17aff0c2c4fcb735fc56e09a1e1fb
Flo Slv <florent3969@hotmail.fr>
2022-10-25 11:39:22 +0200

    🟦 feat: big mobile and small tablet css done !

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
 					Je suis actuellement à la recherche d'un nouvel emploi.
 				</p>
 				<p>
-					Vous pouvez me contacter sur <a href='https://www.linkedin.com/in/flo-slv' target='_blank' rel='noopener noreferrer' aria-label='Linkedin'>Linkedin</a> ou directement par <a href='mailto:florent.sanlaville@protonmail.ch' rel='noopener noreferrer' aria-label='Email'>e-mail</a>.
+					Vous pouvez me contacter sur <a href='https://www.linkedin.com/in/flo-slv' target='_blank' rel='noopener noreferrer' aria-label='Linkedin'>Linkedin</a> ou par <a href='mailto:florent.sanlaville@protonmail.ch' rel='noopener noreferrer' aria-label='Email'>e-mail</a>.
 				</p>
 			</div>
 		</section>
