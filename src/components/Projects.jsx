52afa666d8b768fb7274ebfb4369581e31d8d121
Flo Slv <florent3969@hotmail.fr>
2022-10-26 20:10:55 +0200

    🟦 feat: map through projects object instead of coding 1 by 1

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,34 @@ import SideProject from './utils/SideProject.jsx';
 import '../css/projects.css';
 
 const Projects = () => {
+	const projects = [
+		{
+			side: 'left',
+			imgSource: 'link-in-bio.png',
+			title: 'Link-in-bio',
+			github: 'link-in-bio',
+			text: 'Une page web pour afficher mes réseaux sociaux, à la manière de Linktree.',
+			languages: ['HTML', 'CSS', 'JavaScript', 'Vanta.js']
+		},
+		{
+			side: 'right',
+			imgSource: 'browser.png',
+			title: 'Browser homepage',
+			github: 'browser-homepage',
+			text: "Une page web customisé quand j'ouvre mon navigateur",
+			languages: ['HTML', 'CSS']
+		},
+		{
+			side: 'left',
+			imgSource: 'flo-slv.png',
+			title: 'flo-slv.dev',
+			link: 'https://flo-slv.dev',
+			github: 'website',
+			text: "Mon site web personnel. Créé avec Create React App pour la base. Dans le but d'afficher ma présence sur internet.",
+			languages: ['HTML', 'CSS', 'JavaScript', 'React.js']
+		}
+	];
+
 	return (
 		<section id='projects'>
 			<TitleSection
@@ -14,34 +42,18 @@ const Projects = () => {
 				name={'projects'}
 			/>
 
-			<SideProject
-				side={'left'}
-				imgSource={'link-in-bio.png'}
-				title={'Link-in-bio'}
-				link={'https://links.flo-slv.dev'}
-				github={'link-in-bio'}
-				text={'Une page web pour afficher mes réseaux sociaux, à la manière de Linktree.'}
-				languages={['HTML', 'CSS', 'JavaScript', 'Vanta.js']}
-			/>
-
-			<SideProject
-				side={'right'}
-				imgSource={'browser.png'}
-				title={'Browser homepage'}
-				github={'browser-homepage'}
-				text={"Une page web customisé quand j'ouvre mon navigateur"}
-				languages={['HTML', 'CSS']}
-			/>
-
-			<SideProject
-				side={'left'}
-				imgSource={'flo-slv.png'}
-				title={'flo-slv.dev'}
-				link={'https://flo-slv.dev'}
-				github={'website'}
-				text={"Mon site web personnel. Créé avec Create React App pour la base. Dans le but d'afficher ma présence sur internet."}
-				languages={['HTML', 'CSS', 'JavaScript', 'React.js']}
-			/>
+			{projects.map((p, i) => (
+				<SideProject 
+					key={i}
+					side={p.side}
+					imgSource={p.imgSource}
+					title={p.title}
+					link={p.link}
+					github={p.github}
+					text={p.text}
+					languages={p.languages}
+				/>
+			))}
 		</section>
 	);
 };
